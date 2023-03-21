aac7ff1bac281ced34ddd85c03f0d8bd423f9236
bigsmsdev1 <bigsmsdev1@bigsms.com.au>
2023-03-21 11:52:08 +1100

    Added update page and funtion

--- backend/app.js
+++ backend/app.js
@@ -6,17 +6,15 @@ const port = 3000;
 
 // Set view engine to ejs
 app.set('view engine', 'ejs');
-// This line is needed to load bootstrap correctly
-app.use(express.static("views"));
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 // Create a connection pool using the connection information provided on bit.io.
 const pool = new Pool({
-  user: '', // Username, Leave as Matt-Bruce111 for now
+  user: 'Matt-Bruce111', // Username, Leave as Matt-Bruce111 for now
   host: 'db.bit.io', // Always db.bit.io
-  database: '', // public database 
-  password: '', // key from bit.io database page connect menu
+  database: 'Matt-Bruce111/inv1', // public database 
+  password: 'v2_3zqF7_ptTKqKXWCFasAWNRcdeXPxU', // key from bit.io database page connect menu
   port: 5432,
   ssl: true,
 });
@@ -24,7 +22,7 @@ const pool = new Pool({
 app.get('/', async (req, res) => {
   // var invData = await getInvData();
   // ### Currently not displaying the data in the html page, believed to be an async issue as the page is being loaded before the data is fetched from bit.io
-  res.render('viewall.ejs', { data: await getInvData() });
+  res.render('index.ejs', { data: await getInvData() });
 });
 
 app.get('/view', async (req, res) => {
@@ -42,6 +40,14 @@ app.post('/delete_confirm', async (req, res) => {
   res.redirect('/');
 });
 
+app.post('/edit_confirm', async (req, res) => {
+  var body = req.body;
+  await editItemData(body.itemDesc. body.category, body.condition, body.possession, body.qty, body.id);
+  res.redirect('/');
+});
+app.get('/edit', async (req, res) => {
+  res.render('edit.ejs', { data: await getItemData(req.query.id) });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
@@ -52,6 +58,17 @@ async function getInvData(){
   // Select everything from inventory table
   try{
   invData =  (await pool.query('SELECT * FROM "inventory";' )).rows;}
+  catch(e){
+    throw e;
+  }
+  return invData;
+}
+
+async function editItemData(itemDesc, category, condition, possession, qty, id){
+  var invData = [];
+  // Select everything from inventory table
+  try{
+  invData =  (await pool.query(`UPDATE 'inventory' SET 'item_desc' = ${itemDesc}, 'category' = ${category}, 'condition' = ${condition}, 'possession' = ${possession}, 'qty' = ${qty} WHERE 'id' = ${id} ;` )).rows;}
   catch(e){
     throw e;
   }
